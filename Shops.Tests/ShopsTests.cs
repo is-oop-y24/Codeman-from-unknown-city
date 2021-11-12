e70200b52e34754dfa3e3ba48a96f3250ad7e137
sergei <i_truschev@mail.ru>
2021-11-12 19:24:28 +0300

    fix: remove unnecessary test case

--- Shops.Tests/ShopsTests.cs
+++ Shops.Tests/ShopsTests.cs
@@ -28,14 +28,14 @@ namespace Shops.Tests
             _notExistedProductId = Guid.NewGuid();
             _testProductId = _productsNames.Register("beer");
             _testShop = shopFactory
-                                   .CreateShop(TestShopName, "Street 4, 1")
-                                   .AddProductsInAssortment(new List<(Guid, float)>
-                                   {
-                                       (_testProductId, TestProductCost)
-                                   });
+                .CreateShop(TestShopName, "Street 4, 1")
+                .AddProductsInAssortment(new List<(Guid, float)>
+                {
+                    (_testProductId, TestProductCost)
+                });
             _shopsList = new List<IShop>
             {
-                
+
                 _testShop,
                 shopFactory.CreateShop("Krasnoe&Beloe", "Street 43, 53"),
                 shopFactory.CreateShop("Pyaterochka", "Street 23, 110"),
@@ -49,9 +49,10 @@ namespace Shops.Tests
             const float breadCost = 20;
             try
             {
-                _testShop = _testShop.AddProductsInAssortment(new List<(Guid, float)> { (breadId, breadCost) });
+                _testShop = _testShop.AddProductsInAssortment(new List<(Guid, float)> {(breadId, breadCost)});
                 Assert.True(_testShop.GetCost(breadId) == breadCost);
-            } catch (ShopsException)
+            }
+            catch (ShopsException)
             {
                 Assert.Fail("Product wasn't added");
             }
@@ -64,8 +65,8 @@ namespace Shops.Tests
             Guid meatId = _productsNames.Register("Meat");
             const uint breadCount = 10;
             const uint meatCount = 20;
-            _testShop = _testShop.AddProductsInAssortment(new List<(Guid, float)> { (breadId, 25), (meatId, 40) });
-            _testShop = _testShop.TakeDelivery(new List<(Guid, uint)> { (breadId, breadCount), (meatId, meatCount) });
+            _testShop = _testShop.AddProductsInAssortment(new List<(Guid, float)> {(breadId, 25), (meatId, 40)});
+            _testShop = _testShop.TakeDelivery(new List<(Guid, uint)> {(breadId, breadCount), (meatId, meatCount)});
             Assert.True(breadCount == _testShop.GetCount(breadId) && meatCount == _testShop.GetCount(meatId));
         }
 
@@ -83,17 +84,18 @@ namespace Shops.Tests
             const float moneyBefore = 2430;
             const int nPurchasedProducts = 40;
             const int nProductsAfter = nProductsBefore - nPurchasedProducts;
-            const float moneyAfter = moneyBefore - nPurchasedProducts * TestProductCost; 
+            const float moneyAfter = moneyBefore - nPurchasedProducts * TestProductCost;
             _testShop = _testShop.TakeDelivery(new List<(Guid, uint)> {(_testProductId, nProductsBefore)});
             var customer = new Customer("Vova", moneyBefore);
-            (_testShop, customer) = Deal.Resolve(_testShop, customer, new List<(Guid, uint)> {(_testProductId, nPurchasedProducts)});
+            (_testShop, customer) = Deal.Resolve(_testShop, customer,
+                new List<(Guid, uint)> {(_testProductId, nPurchasedProducts)});
             Assert.True(nProductsAfter == _testShop.GetCount(_testProductId) && moneyAfter == customer.Money);
         }
 
         [Test]
         public void SellProducts_InsufficientFunds_ThrowException()
         {
-            _testShop = _testShop.TakeDelivery(new List<(Guid, uint)> { (_testProductId, 100) });
+            _testShop = _testShop.TakeDelivery(new List<(Guid, uint)> {(_testProductId, 100)});
             var customer = new Customer("Vova", 100);
             Assert.Catch<ShopsException>(() =>
                 Deal.Resolve(_testShop, customer, new List<(Guid, uint)> {(_testProductId, 40)})
@@ -105,7 +107,7 @@ namespace Shops.Tests
         {
             var customer = new Customer("Vova", float.MaxValue);
             Assert.Catch<ShopsException>(() =>
-                Deal.Resolve(_testShop, customer, new List<(Guid, uint)> { (_testProductId, 1) })
+                Deal.Resolve(_testShop, customer, new List<(Guid, uint)> {(_testProductId, 1)})
             );
         }
 
@@ -114,60 +116,8 @@ namespace Shops.Tests
         {
             var customer = new Customer("Vova", 100000);
             Assert.Catch<ShopsException>(() =>
-                Deal.Resolve(_testShop, customer, new List<(Guid, uint)> { (_notExistedProductId, 1) })
+                Deal.Resolve(_testShop, customer, new List<(Guid, uint)> {(_notExistedProductId, 1)})
             );
-        }
-
-        [Test]
-        public void FindShopWithSmallestProductCost_NotFound()
-        {
-            float smallestCost = float.MaxValue;
-            IShop shopWithSmallestCost = null;
-
-            foreach (IShop shop in _shopsList)
-            {
-                if (!shop.Contains(_notExistedProductId))
-                        continue;;
-                float cost = shop.GetCost(_notExistedProductId);
-                if (cost < smallestCost)
-                {
-                    shopWithSmallestCost = shop;
-                    smallestCost = cost;
-                }
-            }
-
-            Assert.True(shopWithSmallestCost == null);
-        }
-
-        [Test]
-        public void FindShopWithSmallestProductCost_FoundRightShop()
-        {
-            float counter = 0;
-
-            var shopsWithoutTestProduct = _shopsList.Where(shop => shop.Name != _testShop.Name).ToList();
-            foreach (IShop shop in shopsWithoutTestProduct)
-            {
-                _shopsList.Remove(shop);
-                _shopsList.Add(shop.AddProductsInAssortment(new List<(Guid, float)>
-                {
-                    (_testProductId, TestProductCost + ++counter)
-                }));
-            }
-
-            IShop rightShopWithSmallestCost = _shopsList.Find(shop => shop.Name == TestShopName);
-            float smallestCost = float.MaxValue;
-            IShop shopWithSmallestCost = null;
-
-            foreach (IShop shop in _shopsList)
-            {
-                float cost = shop.GetCost(_testProductId);
-                if (cost >= smallestCost) 
-                    continue;
-                shopWithSmallestCost = shop;
-                smallestCost = cost;
-            }
-
-            Assert.True(shopWithSmallestCost == rightShopWithSmallestCost);
         }
     }
 }
