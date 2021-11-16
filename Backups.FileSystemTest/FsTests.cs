d4ebc826eabaaeb41a8af3a04fc7e1d85da0cf03
sergei <i_truschev@mail.ru>
2021-11-16 21:10:39 +0300

    feat: add file system tests

---
+++ Backups.FileSystemTest/FsTests.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Backups.FileSystemTest
+{
+    public static class FsTests
+    {
+        private const string TestFilesDirPath = "/home/sergei/RiderProjects/Codeman-from-unknown-city/Backups";
+        
+        public static void TestSingleStorageBackup()
+        {
+            const string repositoryName = ".test_backup_single";
+            IBackupJob backupJob = new BackupJob(
+                new JobObject(),
+                new RepositoryWithSingleStorageAlgorithm(repositoryName, new StorageFactory())
+            );
+            var paths = new List<string>
+            {
+                Path.Combine(TestFilesDirPath, "Program.cs"),
+                Path.Combine(TestFilesDirPath, "Storage.cs"),
+                Path.Combine(TestFilesDirPath, "RestorePoint.cs")
+            };
+            paths.ForEach(action: path =>
+            {
+                if (!backupJob.Add(path))
+                    throw new ApplicationException($"Can't add '{path}' to the backup job. (The file doesn't exists)");
+            });
+            backupJob.Run();
+            backupJob.Run();
+            if (Directory
+                .GetFiles(repositoryName, $"{RepositoryWithSingleStorageAlgorithm.DefaultCopyName}*.zip")
+                .Length != 2)
+            {
+                Directory.Delete(repositoryName, true);
+                throw new ApplicationException("Backup files weren't created");
+            }
+
+            Directory.Delete(repositoryName, true);
+        }
+
+        public static void TestSplitStorageBackup()
+        {
+            const string repositoryName = ".test_backup_split";
+            const int nCopiesOfFirst = 1;
+            const int nCopiesOfLast = 2;
+            IBackupJob backupJob = new BackupJob(
+                new JobObject(),
+                new RepositoryWithSplitStoragesAlgorithm(repositoryName, new StorageFactory())
+            );
+            var testFilesNames = new List<string> {"Program", "Storage"};
+            List<string> paths = testFilesNames.Select(name => Path.Combine(TestFilesDirPath, $"{name}.cs")).ToList();
+            paths.ForEach(action: path =>
+            {
+                if (!backupJob.Add(path))
+                    throw new ApplicationException($"Can't add '{path}' to the backup job. (The file doesn't exists)");
+            });
+            backupJob.Run();
+            backupJob.Remove(paths.First());
+            backupJob.Run();
+            string[] copiesOfFirst = Directory.GetFiles(repositoryName, $"{testFilesNames.First()}*.zip");
+            string[] copiesOfLast = Directory.GetFiles(repositoryName, $"{testFilesNames.Last()}*.zip");
+            if (copiesOfFirst.Length != nCopiesOfFirst || copiesOfLast.Length != nCopiesOfLast) {
+                Directory.Delete(repositoryName, true);
+                throw new ApplicationException("Backup files weren't created");
+            }
+            Directory.Delete(repositoryName, true);
+        }
+    }
+}
---
+++ Backups.FileSystemTest/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Backups.FileSystemTest
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            FsTests.TestSingleStorageBackup();
+            FsTests.TestSplitStorageBackup();
+        }
+    }
+}
