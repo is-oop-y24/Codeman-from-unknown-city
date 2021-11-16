8ffd17cc0b4a94ca4caef098a977be8d9d60ff54
sergei <i_truschev@mail.ru>
2021-11-16 21:10:05 +0300

    feat: add tests

---
+++ Backups.Tests/BackupTest.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Backups.Tests
+{
+    public class BackupTest
+    {
+        private const string SplitStorageBackupJobName = ".test_backup_split";
+        private const string TestFilesDirPath = "/home/sergei/RiderProjects/Codeman-from-unknown-city/Backups";
+        
+        private IBackupJob _splitStorageBackupJob;
+        
+        [SetUp]
+        public void Setup()
+        {
+            _splitStorageBackupJob = new BackupJob(
+                new JobObject(),
+                new RepositoryWithSplitStoragesAlgorithm(SplitStorageBackupJobName, new StorageFactory())
+            );
+        }
+
+        [Test]
+        public void SplitStoragesBackup_CreatedRightNumberOfEntities()
+        {
+            const int nRestorePoints = 2;
+            const int nStorages = 3;
+            var paths = new List<string>
+            {
+                Path.Combine(TestFilesDirPath, "Program.cs"),
+                Path.Combine(TestFilesDirPath, "Storage.cs")
+            };
+            paths.ForEach(path => Assert.True(_splitStorageBackupJob.Add(path)));
+            _splitStorageBackupJob.Run();
+            _splitStorageBackupJob.Remove(Path.Combine(TestFilesDirPath, "Program.cs"));
+            _splitStorageBackupJob.Run();
+            var backup = _splitStorageBackupJob.Backup.ToList();
+            Assert.True(backup.Count() == nRestorePoints);
+            Assert.True(nStorages == backup.Sum(restorePoint => restorePoint.Storages.Count()));
+        }
+        
+        [TearDown]
+        public void Cleanup()
+        {
+            Directory.Delete(SplitStorageBackupJobName, true);
+        }
+    }
+}
